3ff0ea21c96df677532205bfcc912c5187f1ad67
D3spina <61208907+D3spina@users.noreply.github.com>
2021-09-14 17:57:37 +0200

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,5 @@
 import random
+# Création des variables
 reponse = None
 interval = None
 a = None
@@ -8,7 +9,9 @@ reponse_reboot = None
 print("Devinez le chiffre entre a et b!")
 a = int(input("définisser la valeur de a : \n"))
 b = int(input("définisser la valeur de b : \n"))
+# définiton du nombre random à deviner
 nombre_a_deviner = random.randint(a, b)
+# code principal du jeu dans une boucle
 while reponse != nombre_a_deviner or reboot:
 	reponse = int(input("Quelle est votre réponse ?\n"))
 	if reponse > nombre_a_deviner:
@@ -17,6 +20,7 @@ while reponse != nombre_a_deviner or reboot:
 		print("Ce n'est pas ça. Le nombre est plus grand")
 	else:
 		print("Bonne réponse ")
+		# choix de recommencer ou non le jeu, et donc réinitialiser la boucle principale
 		reponse_reboot = input("Souhaitez vous recommencer ? Y ou N\n")
 		if reponse_reboot == "Y":
 			reboot = True
@@ -26,5 +30,6 @@ while reponse != nombre_a_deviner or reboot:
 				b = int(input("définisser la valeur de b : \n"))
 			nombre_a_deviner = random.randint(a, b)
 		else:
+			#sortie du jeu
 			reboot = False
 print("Merci d'avoir joué")
